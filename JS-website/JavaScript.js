e1aad25bb7010f2c97fab4dc6792bcb3271e2c89
Anchor <jim383895a@gmail.com>
2023-07-10 10:38:11 +0800

    javascript edit

--- JS-website/JavaScript.js
+++ JS-website/JavaScript.js
@@ -1,52 +1,57 @@
 /**
  *  Author: Anchor
- *  Date: 2023/07/07
+ *  Date: 2023/07/10
  *  Fetch member api & filter 
  */
 
 document.addEventListener('DOMContentLoaded', () => {
-    let userData;
-// fetch member data
-fetch('./public/data.json')
-.then(response => response.json())
-.then(data => { 
-        console.log(data)
-        userData = data;
+    let updateData;
+    fetch('./public/data.json')
+    .then(response => response.json())
+    .then(data => {
+        updateData = data;
+        console.log(updateData);  
         updateDisplay();
-})
-.catch(error => console.error('Error', error))
-
-const locationSelect = document.getElementById('search-bar__location-select')
-const genderSelect = document.getElementById('search-bar__gender-select')
-locationSelect.addEventListener('change', updateDisplay)
-genderSelect.addEventListener('change', updateDisplay)
-
-function updateDisplay() {
-    const main = document.getElementById('main');
-    main.innerHTML = '';
-    const selectedLocation = locationSelect.value;
-    const selectedGender = genderSelect.value;
-    
-    let filteredData = userData;
-
-    if (selectedLocation !== '未填') {
-        filteredData = filteredData.filter(user => user.Location === selectedLocation)
-    }
-    if (selectedGender !== '未填') {
-        filteredData = filteredData.filter(user => user.Gender === selectedGender)
-    }
-
-    filteredData.map(user => {
-            main.innerHTML += `
-            <div class="main-member">    
-            <p class="main-member__name">${user.Name}</p>
-            <div class="main-member__data">
-            <p class="main-member__phone">${user.Phone}</p>
-            <p class="main-member__gender">${user.Gender}</p>
-            <p class="main-member__location">${user.Location}</p>
-            </div>
-            </div>
-            `
     })
-}
+    .catch(error => console.error('Error', error))
+    
+    const genderSelect = document.getElementById('search-bar__gender-select')
+    const locationSelect = document.getElementById('search-bar__location-select')
+    
+    genderSelect.addEventListener('change', updateDisplay)
+    locationSelect.addEventListener('change', updateDisplay)
+    
+    function updateDisplay() {
+        const main = document.getElementById('main');
+        main.innerHTML = '';
+        
+        const selectedGender = genderSelect.value
+        const selectedLocation = locationSelect.value
+        
+        let selectedData = updateData;
+    
+        console.log(this);
+        console.log(selectedGender);
+        console.log(selectedLocation);
+        
+        if(selectedGender !== '未填') {
+            selectedData = updateData.filter(user => user.Gender === selectedGender)
+    }
+    if(selectedLocation !== '未填') {
+            selectedData = updateData.filter(user => user.Location === selectedLocation)
+    }
+    
+    selectedData.map(user => { 
+        main.innerHTML += `
+        <div class="main-member">    
+        <p class="main-member__name">${user.Name}</p>
+        <div class="main-member__data">
+        <p class="main-member__phone">${user.Phone}</p>
+        <p class="main-member__gender">${user.Gender}</p>
+        <p class="main-member__location">${user.Location}</p>
+        </div>
+        </div>
+        `
+    })  
+    }
 })
