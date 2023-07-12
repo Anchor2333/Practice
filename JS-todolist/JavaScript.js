0a3fe26abb76a4a3d895fb97d07184c30adec1c8
Anchor <jim383895a@gmail.com>
2023-07-12 17:33:23 +0800

    fix bug

--- JS-todolist/JavaScript.js
+++ JS-todolist/JavaScript.js
@@ -2,7 +2,9 @@ const todoList = document.querySelector('#todo__list');
 const todoForm = document.querySelector('#todo__list-form');
 const scrollIcon = document.querySelector('#todo__scroll-icon');
 const totalTasks = document.querySelector('#todo__tasks-total-number');
+let localTodos = JSON.parse(localStorage.getItem('todos')) || [];
 
+/*
 //fetch todos API
 async function fetchData() {
     try {
@@ -17,70 +19,100 @@ async function fetchData() {
     }
 }
 
-//render todolist
-async function renderTodoList() {
+async function fetchStorage() {
     const data = await fetchData();
     const dataDeepCopy = JSON.parse(JSON.stringify(data));
     console.log(dataDeepCopy);
+    const filterData =  dataDeepCopy.todos.filter(data => data.todo !== localTodos.todo);
+    localTodos.concat(filterData);
+}
+*/
 
-    dataDeepCopy.todos.forEach(todoItem => {
+//render todolist
+function renderTodoList() {
+    localTodos.forEach(todoItem => {
         todoForm.innerHTML += `
-        <li class="todo__list-item">
+        <li class="todo__list-item" id="${todoItem.id}">
         <input type="checkbox" class="todo__list-check" name="" id="" ${todoItem.completed ? 'checked' : "" }> 
         <span class="todo__list-text">${todoItem.todo}</span>
         <span class="todo__list-time">${timeUpdate()}</span>
-        <a id="todo__list-delete" class="todo__list-delete">X</a>
+        <a class="todo__list-delete">X</a>
         </li>
         `
+        document.querySelector(".todo__list-delete").addEventListener("click", deleteTodo);
     });
     const totalItems = document.querySelectorAll('.todo__list-item');
     totalTasks.textContent = `${totalItems.length}`;
 }
-document.addEventListener('DOMContentLoaded', renderTodoList);
+document.addEventListener('DOMContentLoaded', function() {
+    // fetchStorage();
+    renderTodoList();    
+});
 
 //add new todo 
 function newTodo() {
     let totalItemsUpdate = document.querySelectorAll('.todo__list-item');
     let inputString = document.querySelector('#todo__input-text');
+    const totalItems = document.querySelectorAll('.todo__list-item');
 
     if (inputString.value !== '') {
         todoForm.innerHTML += `
-        <li class="todo__list-item">
+        <li class="todo__list-item" id="${Date.now().toString()}">
         <input type="checkbox" class="todo__list-check" name="" id=""> 
         <span class="todo__list-text">${inputString.value}</span>
         <span class="todo__list-time">${timeUpdate()}</span>
-        <a id="todo__list-delete" class="todo__list-delete">X</a>s
+        <a class="todo__list-delete">X</a>
         </li>
         `
     };
     inputString.value = '';
-
+    
     if (todoList.scrollHeight > todoList.clientHeight) {
         scrollIcon.style.display = 'block';
     };
     totalTasks.textContent = `${totalItemsUpdate.length + 1}`;
+    updateLocalstorage();
 }
 document.querySelector("#todo__add-button").addEventListener("click", newTodo);
 
 //delete todolist funciton & local strage
-function deleteTodo(event) {
-    const itemId = event.target
+function deleteTodo() {
+    console.log('123');
+    /*
+    localStorage.removeItem(target.id)
+    let totalItemsUpdate = document.querySelectorAll('.todo__list-item');
+    totalTasks.textContent = `${totalItemsUpdate.length}`;
+    */
+   renderTodoList();
 }
 
 //time update
 function timeUpdate() {
     let hours = new Date().getHours();
     let minutes = new Date().getMinutes();
-
+    
     if (hours < 10) {
         hours = '0' + hours;
     };
-
+    
     if (minutes < 10) {
         minutes = '0' + minutes;
     };
     let timeString = hours + ':' + minutes;
     return timeString;
+}
+
+//local storage upadte
+function updateLocalstorage() {
+    const todos = Array.from(document.querySelectorAll('.todo__list-item')).map(todo =>{
+        return {
+            'id' : todo.id,
+            'todo' : todo.querySelector('.todo__list-text').innerText,
+            'completed' : todo.querySelector('.todo__list-check').checked,
+            'time' : todo.querySelector('.todo__list-time').innerText
+        };
+    });
+    localStorage.setItem('todos', JSON.stringify(todos));
 }
 
 //scroll icon update
