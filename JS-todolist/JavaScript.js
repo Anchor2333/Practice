13cf8b69c7c1f05e48363ff68768d52d0eb40ddd
Anchor <jim383895a@gmail.com>
2023-07-12 14:15:44 +0800

    Refactor time update function

--- JS-todolist/JavaScript.js
+++ JS-todolist/JavaScript.js
@@ -3,7 +3,7 @@ const todoForm = document.querySelector('#todo__list-form');
 const scrollIcon = document.querySelector('#todo__scroll-icon');
 const totalTasks = document.querySelector('#todo__tasks-total-number');
 
-//fetch todo API
+//fetch todos API
 async function fetchData() {
     try {
         let response = await fetch('https://dummyjson.com/todos');
@@ -11,36 +11,25 @@ async function fetchData() {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         let data = await response.json();
-        return data
+        return data;
     } catch (error) {
         console.log('Fail to fetch todo list:', error);
     }
 }
 
-//Load display
+//render todolist
 async function renderTodoList() {
     const data = await fetchData();
-    const dataDeepCopy = JSON.parse(JSON.stringify(data))
+    const dataDeepCopy = JSON.parse(JSON.stringify(data));
     console.log(dataDeepCopy);
-
-    let hours = new Date().getHours();
-    let minutes = new Date().getMinutes();
-
-    if (hours < 10) {
-        hours = '0' + hours;
-    };
-
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    };
-    let timeString = hours + ':' + minutes;
 
     dataDeepCopy.todos.forEach(todoItem => {
         todoForm.innerHTML += `
         <li class="todo__list-item">
         <input type="checkbox" class="todo__list-check" name="" id="" ${todoItem.completed ? 'checked' : "" }> 
         <span class="todo__list-text">${todoItem.todo}</span>
-        <span class="todo__list-time">${timeString}</span>
+        <span class="todo__list-time">${timeUpdate()}</span>
+        <a id="todo__list-delete" class="todo__list-delete">X</a>
         </li>
         `
     });
@@ -52,17 +41,6 @@ document.addEventListener('DOMContentLoaded', renderTodoList);
 //add new todo 
 function newTodo() {
     let totalItemsUpdate = document.querySelectorAll('.todo__list-item');
-    let hours = new Date().getHours();
-    let minutes = new Date().getMinutes();
-
-    if (hours < 10) {
-        hours = '0' + hours;
-    };
-
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    };
-    let timeString = hours + ':' + minutes;
     let inputString = document.querySelector('#todo__input-text');
 
     if (inputString.value !== '') {
@@ -70,7 +48,8 @@ function newTodo() {
         <li class="todo__list-item">
         <input type="checkbox" class="todo__list-check" name="" id=""> 
         <span class="todo__list-text">${inputString.value}</span>
-        <span class="todo__list-time">${timeString}</span>
+        <span class="todo__list-time">${timeUpdate()}</span>
+        <a id="todo__list-delete" class="todo__list-delete">X</a>s
         </li>
         `
     };
@@ -83,11 +62,31 @@ function newTodo() {
 }
 document.querySelector("#todo__add-button").addEventListener("click", newTodo);
 
+//delete todolist funciton & local strage
+function deleteTodo(event) {
+    const itemId = event.target
+}
+
+//time update
+function timeUpdate() {
+    let hours = new Date().getHours();
+    let minutes = new Date().getMinutes();
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    };
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    };
+    let timeString = hours + ':' + minutes;
+    return timeString;
+}
+
 //scroll icon update
 function scrollEvent() {
     const { scrollTop, scrollHeight, clientHeight } = document.querySelector('#todo__list');
-    if (clientHeight + scrollTop >= scrollHeight) {
-        console.log('牛肉');
+    if (clientHeight + scrollTop >= scrollHeight - 5) {
         document.querySelector('#todo__bottom-icon').style.height = '3px';
         scrollIcon.style.display = 'none';
     } else {
