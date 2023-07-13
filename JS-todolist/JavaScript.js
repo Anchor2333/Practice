a5e1272eff7d9cf82e4a1b3e42d622e70c9ed1f3
Anchor <jim383895a@gmail.com>
2023-07-13 17:34:05 +0800

    Planing refactor update localstorage

--- JS-todolist/JavaScript.js
+++ JS-todolist/JavaScript.js
@@ -14,7 +14,7 @@ async function fetchData() {
         }
 
         const data = await response.json();
-        
+
         return data;
     } catch (error) {
         console.log('Fail to fetch todo list:', error);
@@ -32,22 +32,21 @@ async function renderTodoList() {
         console.log(filterData);
         localTodos = localTodos.concat(filterData);
     } else {
-        localTodos = JSON.parse(localStorage.getItem('todos')) ;
+        localTodos = JSON.parse(localStorage.getItem('todos'));
     }
 
     localStorage.setItem('todos', JSON.stringify(localTodos));
-    updateDisplay() 
-    
+    updateDisplay()
+
     let totalItemsUpdate = document.querySelectorAll('.todo__list-item');
     totalTasks.textContent = `${totalItemsUpdate.length}`;
 }
-document.addEventListener('DOMContentLoaded', renderTodoList);
 
 //add new todo 
 function newTodo() {
     let totalItemsUpdate = document.querySelectorAll('.todo__list-item');
     let inputString = document.querySelector('#todo__input-text');
-    
+
     if (inputString.value !== '') {
         todoForm.innerHTML += `
         <li class="todo__list-item" id="${Date.now().toString()}">
@@ -59,30 +58,22 @@ function newTodo() {
         `
     };
     inputString.value = '';
-    
+
     if (todoList.scrollHeight > todoList.clientHeight) {
         scrollIcon.style.display = 'block';
     };
-    
+
     totalTasks.textContent = `${totalItemsUpdate.length + 1}`;
     updateLocalstorage();
 }
-document.querySelector("#todo__add-button").addEventListener("click", newTodo);
 
 //delete todolist funciton & local strage
 function deleteTodo(todoId) {
     localTodos = JSON.parse(localStorage.getItem('todos'));
-    const index = localTodos.findIndex((i) => i.id === todoId);
     localTodos = localTodos.filter(item => item.id !== todoId);
-    
-    if (index !== -1) {
-        localTodos.splice(index, 1);
-    }
-    
-    console.log(localTodos, index);
     localStorage.setItem('todos', JSON.stringify(localTodos));
     let totalItemsUpdate = document.querySelectorAll('.todo__list-item');
-    totalTasks.textContent = `${ totalItemsUpdate.length - 1 }`;
+    totalTasks.textContent = `${totalItemsUpdate.length - 1}`;
 }
 
 //update display
@@ -91,25 +82,44 @@ function updateDisplay() {
     localTodos = JSON.parse(localStorage.getItem('todos'));
     localTodos.forEach(todoItem => {
         todoForm.innerHTML += `
-        <li class="todo__list-item" id="${ todoItem.id }">
-        <input type="checkbox" class="todo__list-check" name="" id="" ${ todoItem.completed ? 'checked' : "" }> 
+        <li class="todo__list-item" id="${todoItem.id}">
+        <input type="checkbox" class="todo__list-check" name="" id="" ${todoItem.completed ? 'checked' : ""}> 
         <span class="todo__list-text">${todoItem.todo}</span>
-        <span class="todo__list-time">${ todoItem.time ? todoItem.time : timeUpdate() }</span>
+        <span class="todo__list-time">${todoItem.time ? todoItem.time : timeUpdate()}</span>
         <a class="todo__list-delete">X</a>
         </li>
         `
     });
 }
 
+//click list event handler
+function listEventHandler(p) {
+    switch (true) {
+        case p.target.className === 'todo__list-delete':
+            deleteTodo(p.target.parentNode.id);
+            break;
+        case p.target.className === 'todo__list-check':
+            updateLocalstorage();
+            break;
+        case p.target.className === 'todo__list-text':
+            console.log(1);
+            break;
+        default:
+            break;
+    }
+    updateDisplay();
+    updateLocalstorage();
+}
+
 //time update
 function timeUpdate() {
     let hours = new Date().getHours();
     let minutes = new Date().getMinutes();
-    
+
     if (hours < 10) {
         hours = '0' + hours;
     };
-    
+
     if (minutes < 10) {
         minutes = '0' + minutes;
     };
@@ -119,12 +129,12 @@ function timeUpdate() {
 
 //local storage update
 function updateLocalstorage() {
-    const todoList = Array.from(document.querySelectorAll('.todo__list-item')).map(todo =>{
+    const todoList = Array.from(document.querySelectorAll('.todo__list-item')).map(todo => {
         return {
-            'id' : todo.id,
-            'todo' : todo.querySelector('.todo__list-text').innerText,
-            'completed' : todo.querySelector('.todo__list-check').checked,
-            'time' : todo.querySelector('.todo__list-time').innerText
+            'id': todo.id,
+            'todo': todo.querySelector('.todo__list-text').innerText,
+            'completed': todo.querySelector('.todo__list-check').checked,
+            'time': todo.querySelector('.todo__list-time').innerText
         };
     });
     localStorage.setItem('todos', JSON.stringify(todoList));
@@ -141,22 +151,8 @@ function scrollEvent() {
         scrollIcon.style.display = 'block';
     }
 };
+
+document.addEventListener('DOMContentLoaded', renderTodoList);
+document.querySelector("#todo__add-button").addEventListener("click", newTodo);
 document.querySelector('#todo__list').addEventListener('scroll', scrollEvent);
-
-document.querySelector('#todo__list-form').addEventListener('click', function(p) {
-    switch (true) {
-        case p.target.className === 'todo__list-delete': 
-            deleteTodo(p.target.parentNode.id);
-            break;
-        case p.target.className === 'todo__list-check':
-            updateLocalstorage();
-            break;
-        case p.target.className === 'todo__list-text':
-            console.log(1);
-            break;
-        default: 
-            break;
-    }
-
-    updateDisplay();
-})
+document.querySelector('#todo__list-form').addEventListener('click', listEventHandler)
