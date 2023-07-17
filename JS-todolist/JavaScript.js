64793dd609fd24823a7f95ecac8842c1ec16da11
Anchor <jim383895a@gmail.com>
2023-07-17 16:10:14 +0800

    update regex

--- JS-todolist/JavaScript.js
+++ JS-todolist/JavaScript.js
@@ -234,7 +234,7 @@ function scrollIconDisplay() {
 
 // regex input
 function validateInput(inputText) {
-    const regex = /^[a-zA-Z0-9]*([a-zA-Z0-9]+\s*)+[a-zA-Z0-9]*$/;
+    const regex = /^[a-zA-Z0-9\'\" ]*([a-zA-Z0-9]+\s*)+[a-zA-Z0-9']*$/;
     return regex.test(inputText);
 }
 
