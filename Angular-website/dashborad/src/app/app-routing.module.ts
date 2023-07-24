c9aaa1d9100a6c0c0995ec2d58444c4ece3bcab3
Anchor <jim383895a@gmail.com>
2023-07-24 15:55:59 +0800

    sidenav dashboard

--- Angular-website/dashborad/src/app/app-routing.module.ts
+++ Angular-website/dashborad/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProdcutsComponent } from './prodcuts/prodcuts.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { PagesComponent } from './pages/pages.component';
+import { MediaComponent } from './media/media.component';
+import { SettingComponent } from './setting/setting.component';
 
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'products', component: ProdcutsComponent },
+  { path: 'statistics', component: StatisticsComponent },
+  { path: 'coupens', component: CoupensComponent },
+  { path: 'pages', component: PagesComponent },
+  { path: 'media', component: MediaComponent },
+  { path: 'settings', component: SettingComponent }
 ];
 
 @NgModule({
--- Angular-website/dashborad/src/app/app.module.ts
+++ Angular-website/dashborad/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -28,6 +29,7 @@ import { SettingComponent } from './setting/setting.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
