b5e36ce8c31d1abca78804892dd41b91d2e9efcd
Anchor <jim383895a@gmail.com>
2023-08-17 17:33:12 +0800

    Practice RxJs

--- RxJs/src/app/practice/practice.component.ts
+++ RxJs/src/app/practice/practice.component.ts
@@ -1,5 +1,6 @@
+import { newArray } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
-import { fromEvent } from 'rxjs';
+import { fromEvent, Observable, interval } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
@@ -10,15 +11,29 @@ import { filter, map } from 'rxjs/operators';
 export class PracticeComponent implements OnInit {
 
   constructor() { }
+  currentTime$: Observable<string>;
+
+  currentCounts$: Observable<string>;
 
   ngOnInit(): void {
-    fromEvent (document, 'click')
-    .pipe(
-      filter((_, index) => index % 2 === 0),
-      map((event: MouseEvent) => ({ x: event.x, y: event.y }))
-    )
-    .subscribe((position) => {
-      console.log(`x: ${position.x}, y: ${position.y}`);
-    });
+    fromEvent(document, 'click')
+      .pipe(
+        filter((_, index) => index % 2 === 0),
+        map((event: MouseEvent) => ({ x: event.x, y: event.y }))
+      )
+      .subscribe((position) => {
+        console.log(`x: ${position.x}, y: ${position.y}`);
+      });
+
+
+      this.currentTime$ = interval(1000)
+      .pipe(
+        map(() => new Date().toLocaleTimeString())
+      );
+
+      this.currentCounts$ = interval(100)
+      .pipe(
+        map(() => new Date().toLocaleTimeString())
+      );
   }
 }
